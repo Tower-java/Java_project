c03880c0df750f06be4623191885f7b7fd0f4da7
Achraf Hanjaf <achraf.hanjaf@epitech.eu>
2025-11-10 09:40:41 +0100

    Bloc2: systémes de status

---
+++ DefendStatus.java
@@ -0,0 +1,50 @@
+public class DefendStatus extends StatusEffect {
+    private int duration;
+    private int damageBlockAmount;
+
+    public DefendStatus(int duration, int damageBlockAmount) {
+        this.duration = duration;
+        this.damageBlockAmount = damageBlockAmount;
+    }
+
+    @Override
+    public int modifyDamageDealt(int damage) {
+        return damage;
+    }
+
+    @Override
+    public int modifyDamageTaken(int damage) {
+        int reduced = damage - damageBlockAmount;
+        return Math.max(reduced, 0);
+    }
+
+    @Override
+    public void onTurnEnd(Entity target) {
+        // Aucun effet à la fin du tour
+    }
+
+    @Override
+    public void onTurnStart(Entity target) {
+        // Aucun effet au début du tour
+    }
+
+    @Override
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public void updateDuration() {
+        duration--;
+    }
+
+    @Override
+    public boolean isDone() {
+        return duration <= 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Defend";
+    }
+}
